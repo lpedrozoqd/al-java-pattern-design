ce9827c598569cfd287f902273217c3b4302aad6
Leonel Pedrozo Quiroz <leonel.pedrozo.quiroz.d@gmail.com>
2022-06-24 23:34:00 -0500

    v24jun22.2333 - nuevos cambios

--- src/co/clarap/al/allPatternDesign/App.java
+++ src/co/clarap/al/allPatternDesign/App.java
@@ -1,6 +1,6 @@
 package co.clarap.al.allPatternDesign;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Patrones de diseño!!");
     }
 }
