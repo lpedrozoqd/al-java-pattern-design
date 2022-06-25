69648277da4954cf66efd44d6d0e1ff9c449af92
Leonel Pedrozo Quiroz <leonel.pedrozo.quiroz.d@gmail.com>
2022-06-25 09:29:38 -0500

    v25jun22.929 - patron de creación: abstract factory method

---
+++ src/co/clarap/al/allPatternDesign/creacion/abstractFactoryMethod/FabricaVehiculo.java
@@ -0,0 +1,6 @@
+package co.clarap.al.allPatternDesign.creacion.abstractFactoryMethod;
+
+public interface FabricaVehiculo {
+    Automovil createAutomovile(String model,String color, int  potencia, double espacio);
+    Scooter createScooter( String model, String color,int potencia);
+}
---
+++ src/co/clarap/al/allPatternDesign/creacion/abstractFactoryMethod/Scooter.java
@@ -0,0 +1,17 @@
+package co.clarap.al.allPatternDesign.creacion.abstractFactoryMethod;
+
+public abstract class Scooter {
+    protected String modelo;
+    protected String color;
+    protected int potencia;
+
+    public Scooter(String modelo, String color, int potencia) {
+        this.modelo = modelo;
+        this.color = color;
+        this.potencia = potencia;
+    }
+
+    public  abstract  void mostrarCarateristicas();
+
+    
+}
